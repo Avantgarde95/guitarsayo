73397997e10ff5de36b1288d88a69418ad2f6a24
Avantgarde95 <95phm@kaist.ac.kr>
2020-10-23 15:02:59 +0900

    Connect checkbox to search input

--- app/js/Popup.js
+++ app/js/Popup.js
@@ -6,36 +6,52 @@
                 type: 'Market',
                 name: '스쿨뮤직 Schoolmusic',
                 url: 'http://www.schoolmusic.co.kr',
-                searchURL: 'http://www.schoolmusic.co.kr/Shop/index.php3?var=Search&max_cnt=15&keyword=$'
+                search: {
+                    url: 'http://www.schoolmusic.co.kr/Shop/index.php3?var=Search&max_cnt=15&keyword=$',
+                    allow: true
+                }
             },
             {
                 type: 'Market',
                 name: '뮤직포스 Music Force',
                 url: 'http://musicforce.co.kr',
-                searchURL: 'https://musicforce.co.kr/product/search.html?banner_action=&keyword=$'
+                search: {
+                    url: 'https://musicforce.co.kr/product/search.html?banner_action=&keyword=$',
+                    allow: true
+                }
             },
             {
                 type: 'Market',
                 name: '프리버드 Freebud',
                 url: 'http://www.freebud.co.kr',
-                searchURL: 'http://www.freebud.co.kr/shop/goods/goods_search.php?disp_type=&hid_sword=&searched=Y&log=1&sort=&page_num=&delivery=&view=&total=&searchword=&new_goods=&skey=all&sword=$'
+                search: {
+                    url: 'http://www.freebud.co.kr/shop/goods/goods_search.php?disp_type=&hid_sword=&searched=Y&log=1&sort=&page_num=&delivery=&view=&total=&searchword=&new_goods=&skey=all&sword=$',
+                    allow: true
+                }
             },
             {
                 type: 'Market',
                 name: '버즈비 Buzzbee',
                 url: 'http://buzzbee.co.kr',
-                searchURL: 'http://buzzbee.co.kr/shop/goods/goods_search.php?searched=Y&log=1&skey=all&hid_pr_text=&hid_link_url=&edit=&sword=$'
+                search: {
+                    url: 'http://buzzbee.co.kr/shop/goods/goods_search.php?searched=Y&log=1&skey=all&hid_pr_text=&hid_link_url=&edit=&sword=$',
+                    allow: true
+                }
             },
             {
                 type: 'Community',
                 name: '뮬 Mule',
                 url: 'https://www.mule.co.kr/',
-                searchURL: 'https://www.mule.co.kr/bbs/market/sell?page=1&map=list&mode=list&region=&start_price=&end_price=&qf=title&qs=$&category=&ct1=&ct2=&ct3=&store=&options=&soldout=&sido=&gugun=&dong=&period=6&of=wdate&od=desc&v=l'
+                search: {
+                    url: 'https://www.mule.co.kr/bbs/market/sell?page=1&map=list&mode=list&region=&start_price=&end_price=&qf=title&qs=$&category=&ct1=&ct2=&ct3=&store=&options=&soldout=&sido=&gugun=&dong=&period=6&of=wdate&od=desc&v=l',
+                    allow: false
+                }
             },
             {
                 type: 'Community',
                 name: '일렉트릭기타 마이너 갤러리',
-                url: 'https://gall.dcinside.com/mgallery/board/lists?id=electricguitar'
+                url: 'https://gall.dcinside.com/mgallery/board/lists?id=electricguitar',
+                search: null
             }
         ],
         marketsArea = document.getElementsByClassName('MarketsArea')[0],
@@ -56,8 +72,8 @@
         }
 
         sites.forEach(function (site) {
-            if (site.hasOwnProperty('searchURL')) {
-                openURL(site.searchURL.replace('$', encodedQuery));
+            if (site.search !== null && site.search.allow) {
+                openURL(site.search.url.replace('$', encodedQuery));
             }
         });
     }
@@ -77,7 +93,10 @@
 
         searchOptionCheckbox.className = 'SearchOptionCheckbox';
         searchOptionCheckbox.type = 'checkbox';
-        searchOptionCheckbox.checked = true;
+
+        searchOptionCheckbox.addEventListener('change', function () {
+            site.search.allow = searchOptionCheckbox.checked;
+        });
 
         searchOptionLabel.className = 'SearchOptionLabel';
         searchOptionLabel.innerHTML = '검색';
@@ -86,7 +105,8 @@
         linkArea.className = 'LinkArea';
         linkArea.appendChild(linkButton);
 
-        if (site.hasOwnProperty('searchURL')) {
+        if (site.search !== null) {
+            searchOptionCheckbox.checked = site.search.allow;
             linkArea.appendChild(searchOptionLabel);
         }
 
