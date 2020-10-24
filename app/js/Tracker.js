551c4e448af090384f4fbd8721dc9299d0b32c8f
Avantgarde95 <95phm@kaist.ac.kr>
2020-10-24 17:15:39 +0900

    Contenet security policy for Google Analytics

---
+++ app/js/Tracker.js
@@ -0,0 +1,10 @@
+// Google Analytics.
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-22G5E0LPFP');
